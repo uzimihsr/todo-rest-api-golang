25092c6bf7f6e4c876826d2bd0833cca760b928c
uzimihsr <uzimihsr@gmail.com>
2021-06-20 00:14:00 +0900

    implement handler

--- presentation/handler/todo_handler.go
+++ presentation/handler/todo_handler.go
@@ -1,8 +1,12 @@
 package handler
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
+	"strconv"
 
+	"github.com/gorilla/mux"
 	"github.com/uzimihsr/todo-rest-api-golang/usecase/service"
 )
 
@@ -18,41 +22,147 @@ type toDoHandler struct {
 	service service.ToDoService
 }
 
-func NewToDoHandler() ToDoHandler {
-	return new(toDoHandler)
+func NewToDoHandler(service service.ToDoService) ToDoHandler {
+	return &toDoHandler{
+		service: service,
+	}
 }
 
 func (h *toDoHandler) Create() http.HandlerFunc {
-	// WIP
 	return func(w http.ResponseWriter, r *http.Request) {
-		http.Error(w, "ERROR! Create ToDo API is still a work in progress.", http.StatusInternalServerError)
+		requestToDo, err := parseRequestJSON(r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		resultToDo, err := h.service.Create(requestToDo)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(resultToDo)
 	}
 }
 
 func (h *toDoHandler) Read() http.HandlerFunc {
-	// WIP
 	return func(w http.ResponseWriter, r *http.Request) {
-		http.Error(w, "ERROR! Read ToDo API is still a work in progress.", http.StatusInternalServerError)
+		id, err := getPathParamId(r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		requestToDo := &service.ToDoObject{
+			Id: id,
+		}
+
+		resultToDo, err := h.service.Read(requestToDo)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(resultToDo)
 	}
 }
 
 func (h *toDoHandler) Update() http.HandlerFunc {
 	// WIP
 	return func(w http.ResponseWriter, r *http.Request) {
-		http.Error(w, "ERROR! Update ToDo API is still a work in progress.", http.StatusInternalServerError)
+		id, err := getPathParamId(r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		requestToDo, err := parseRequestJSON(r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		requestToDo.Id = id
+
+		// DBのレコードを更新
+		resultToDo, err := h.service.Update(requestToDo)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(resultToDo)
 	}
 }
 
 func (h *toDoHandler) Delete() http.HandlerFunc {
-	// WIP
 	return func(w http.ResponseWriter, r *http.Request) {
-		http.Error(w, "ERROR! Delete ToDo API is still a work in progress.", http.StatusInternalServerError)
+		id, err := getPathParamId(r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		requestToDo := &service.ToDoObject{
+			Id: id,
+		}
+
+		// DBのレコードを削除
+		resultToDo, err := h.service.Delete(requestToDo)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(resultToDo)
 	}
 }
 
 func (h *toDoHandler) List() http.HandlerFunc {
-	// WIP
 	return func(w http.ResponseWriter, r *http.Request) {
-		http.Error(w, "ERROR! List ToDo API is still a work in progress.", http.StatusInternalServerError)
+		done := r.FormValue("done")
+		listOption := &service.ListOption{
+			Done: done,
+		}
+		todoList, err := h.service.List(listOption)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		resultList := []service.ToDoObject{}
+		resultList = append(resultList, todoList...)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(resultList)
 	}
+}
+
+// パスパラメータ{id}を取得する
+func getPathParamId(r *http.Request) (int64, error) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		return -1, err
+	}
+	return int64(id), nil
+}
+
+// リクエストボディをJSONにパースする
+func parseRequestJSON(r *http.Request) (*service.ToDoObject, error) {
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return nil, err
+	}
+	reqObject := &service.ToDoObject{}
+	err = json.Unmarshal(reqBody, reqObject)
+	if err != nil {
+		return nil, err
+	}
+	return reqObject, nil
 }
