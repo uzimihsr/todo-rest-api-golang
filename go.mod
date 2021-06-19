d68d185d2ecbc49a1f161ef56dcb2b6e1b04368f
uzimihsr <uzimihsr@gmail.com>
2021-06-19 18:16:49 +0900

    implement repository

--- go.mod
+++ go.mod
@@ -2,4 +2,19 @@ module github.com/uzimihsr/todo-rest-api-golang
 
 go 1.16
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/DATA-DOG/go-sqlmock v1.5.0
+	github.com/Nvveen/Gotty v0.0.0-20120604004816-cd527374f1e5 // indirect
+	github.com/cenkalti/backoff v2.2.1+incompatible // indirect
+	github.com/containerd/continuity v0.1.0 // indirect
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.4.0 // indirect
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/gorilla/mux v1.8.0
+	github.com/opencontainers/image-spec v1.0.1 // indirect
+	github.com/opencontainers/runc v0.1.1 // indirect
+	github.com/ory/dockertest v3.3.5+incompatible
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
+	golang.org/x/sys v0.0.0-20210616094352-59db8d763f22 // indirect
+)
--- infrastructure/database/todo_repository_mysql.go
+++ infrastructure/database/todo_repository_mysql.go
@@ -1,40 +1,124 @@
 package database
 
 import (
+	"database/sql"
+	"errors"
+	"strconv"
+
 	"github.com/uzimihsr/todo-rest-api-golang/domain/model"
 	"github.com/uzimihsr/todo-rest-api-golang/domain/repository"
 )
 
 // implementation of repository
 type toDoRepositoryMySQL struct {
+	db *sql.DB
 }
 
-func NewToDoRepositoryMySQL() repository.ToDoRepository {
-	t := new(toDoRepositoryMySQL)
-	return t
+func NewToDoRepositoryMySQL(db *sql.DB) repository.ToDoRepository {
+	return &toDoRepositoryMySQL{db: db}
 }
 
-func (todoDB *toDoRepositoryMySQL) Create(model *model.ToDo) (int64, error) {
-	// WIP
-	return 0, nil
+func (r *toDoRepositoryMySQL) Create(model *model.ToDo) (int64, error) {
+	result, err := r.db.Exec(
+		"INSERT INTO todo(title, done) VALUES ( ?, ? )",
+		model.Title,
+		model.Done,
+	)
+	if err != nil {
+		return -1, err
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		return -1, err
+	}
+	return id, nil
 }
 
 func (todoDB *toDoRepositoryMySQL) Read(id int64) (*model.ToDo, error) {
-	// WIP
-	return nil, nil
+	todo := &model.ToDo{}
+	err := todoDB.db.QueryRow(
+		"SELECT id, title, done, created_at, updated_at FROM todo WHERE id = ?",
+		id,
+	).Scan(
+		&todo.Id,
+		&todo.Title,
+		&todo.Done,
+		&todo.CreatedAt,
+		&todo.UpdatedAt,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return todo, nil
 }
 
 func (todoDB *toDoRepositoryMySQL) Update(model *model.ToDo) error {
-	// WIP
+	result, err := todoDB.db.Exec(
+		"UPDATE todo SET title = ?, done = ? WHERE id = ?",
+		model.Title,
+		model.Done,
+		model.Id,
+	)
+	if err != nil {
+		return err
+	}
+	affected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if affected != 1 {
+		return errors.New("UPDATE FAILED")
+	}
 	return nil
 }
 
 func (todoDB *toDoRepositoryMySQL) Delete(id int64) error {
-	// WIP
+	result, err := todoDB.db.Exec("DELETE FROM todo WHERE id = ?", id)
+	if err != nil {
+		return err
+	}
+	affected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if affected != 1 {
+		return errors.New("UPDATE FAILED")
+	}
 	return nil
 }
 
 func (todoDB *toDoRepositoryMySQL) List(selector *model.ToDoSelector) ([]model.ToDo, error) {
-	// WIP
-	return nil, nil
+	var rows *sql.Rows
+	var err error
+	var toDoList []model.ToDo
+
+	if selector == nil {
+		rows, err = todoDB.db.Query("SELECT id, title, done, created_at, updated_at FROM todo")
+		if err != nil {
+			return nil, err
+		}
+	} else if selector.Done != "" {
+		done, _ := strconv.ParseBool(selector.Done)
+		rows, err = todoDB.db.Query("SELECT id, title, done, created_at, updated_at FROM todo WHERE done = ?", done)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	for rows.Next() {
+		todo := model.ToDo{}
+		err := rows.Scan(
+			&todo.Id,
+			&todo.Title,
+			&todo.Done,
+			&todo.CreatedAt,
+			&todo.UpdatedAt,
+		)
+		if err != nil {
+			return nil, err
+		}
+		toDoList = append(toDoList, todo)
+	}
+
+	return toDoList, nil
 }
